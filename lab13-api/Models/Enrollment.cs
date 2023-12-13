5218cb2ff0267d6bc06fb5125335bdcceff08560
Nilto9 <82666300+Nilto9@users.noreply.github.com>
2023-12-13 04:06:43 -0500

    Update Enrollment.cs

--- lab13-api/Models/Enrollment.cs
+++ lab13-api/Models/Enrollment.cs
@@ -3,13 +3,10 @@
     public class Enrollment
     {
         public int EnrollmentID { get; set; }
-
         public Student Student { get; set; }
         public int StudentID { get; set; }
-
         public Course Course { get; set; }
         public int CourseID { get; set; }
         public DateTime date { get; set; }
-
     }
 }
